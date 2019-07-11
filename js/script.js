b034d240242c30c089cf2055d492003b08161eab
Christine-N-Mwaura <njerimwaura25@gmail.com>
2019-07-11 11:29:59 +0300

    This commit will add a js folder, a script file inside it and copied code from my js fiddle.

---
+++ js/script.js
@@ -0,0 +1,6 @@
+function add (number3,number4){
+return number3 + number4;
+}
+var number3 = parseInt(prompt("Enter a number:"));
+var number4 = parseInt(prompt("Enter another number:"));
+alert(add(number3,number4));
